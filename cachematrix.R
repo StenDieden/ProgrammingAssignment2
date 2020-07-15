e991fe39fbc121ad38d977d9edcf006f21968731
Sten Dieden <sten.dieden@actualytics.se>
2020-07-15 12:05:42 +0200

    Submission to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Given a matrix as its argument, the first function creates an object to 
+## store the inverted argument, as well as a set of functions to place and 
+## retrieve the inverted matrix’ in and from “storage”.
+## The second function requires the first function’s output as arguments
+## to either (i) retrieve the inverted matrix – if already stored  - from 
+## storage (cache), or (ii) create and store the inverted matrix.  
 
-## Write a short comment describing this function
+
+##The makeCacheMatrix function defines six sibling objects in its environment;
+## v, y and four "listed" functions. When run with a matrix as input the
+##function creates a environment within the global environment, that hosts six
+## objects accessible to the second function, 
 
 makeCacheMatrix <- function(x = matrix()) {
+    v <- NULL  
+    
+    set <- function(y) {
+      x <<- y
+      v <<- NULL
+    }
+    
+    get <- function() x
+    setinv<- function(inv) v <<- inv
+    getinv <- function() v
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+  }
 
-}
 
+## Taking output from the first function, the solveCacheMatrix function either
+## (i) retrieves the inverted matrix from the "v" cache,if already stored, or 
+## (ii) creates and stores the inverted matrix in v.
 
-## Write a short comment describing this function
+solveCacheMatrix <- function(x, ...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  v <- x$getinv()
+
+## (i)
+    if(!is.null(v)) {
+    message("getting cached data")
+    return(v)
+    }
+
+## (ii)  
+  data <- y$get()
+  v <- solve(data, ...)
+  x$setinv(v)
+  v
 }
